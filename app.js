836d3c7076d28bc6f313a3cf0eb8feba94a342c5
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-06-03 23:02:47 +0100

    `onExit`

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require('babel-register');
 const chalk = require('chalk');
 
 const args = require('./lib/args').default;
-const requestMap = require('./lib/request-map').default;
 
 const {
   PRODUCTION,
@@ -22,47 +21,11 @@ const address = require('./lib/address').default;
 const contact = require('./lib/contact').default;
 const toBool = require('./lib/to-bool').default;
 const format = require('./lib/format').default;
-
-const scenario = args.get('scenario');
-
-const onExit = (code = 0) => {
-  let message;
-  switch (code) {
-    case 130:
-      message = `Execution of scenario '${scenario}' has been stopped.`;
-      break;
-    case 1:
-      message = 'Boom!';
-      break;
-    case 2:
-      message = 'No scenario to execute.';
-      break;
-    case 3:
-      message = `No matching scenario for '${scenario}'.`;
-      break;
-    default:
-      message = `Execution of scenario '${scenario}' is complete.`;
-      break;
-  }
-
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-
-  if (code === 0 || code === 130) Logger.info(message);
-  else Logger.error(message);
-
-  if (toBool(args.get('captureNetwork'))) {
-    const { size = 0 } = requestMap;
-
-    Logger.info('\t', chalk.yellow(format(size)), (size === 1) ? 'Request.' : 'Requests.');
-  }
-
-  process.exitCode = (code === 1)
-    ? 1
-    : 0;
-};
+const onExit = require('./lib/on-exit').default;
 
 process.once('exit', onExit);
+
+const scenario = args.get('scenario');
 
 if (!scenario) process.exit(2);
 
---
+++ src/on-exit.js
@@ -0,0 +1,51 @@
+
+import chalk from 'chalk';
+
+import args from 'app/args';
+
+import Logger from 'app/logger';
+import toBool from 'app/to-bool';
+import format from 'app/format';
+
+import requestMap from 'app/request-map';
+
+const scenario = args.get('scenario');
+
+export default (code = 0) => {
+  let message;
+  switch (code) {
+    case 130:
+      message = `Execution of scenario '${scenario}' has been stopped.`;
+      break;
+    case 1:
+      message = 'Boom!';
+      break;
+    case 2:
+      message = 'No scenario to execute.';
+      break;
+    case 3:
+      message = `No matching scenario for '${scenario}'.`;
+      break;
+    default:
+      message = `Execution of scenario '${scenario}' is complete.`;
+      break;
+  }
+
+  const { stdout } = process;
+
+  stdout.clearLine();
+  stdout.cursorTo(0);
+
+  if (code === 0 || code === 130) Logger.info(message);
+  else Logger.error(message);
+
+  if (toBool(args.get('captureNetwork'))) {
+    const { size = 0 } = requestMap;
+
+    Logger.info('\t', chalk.yellow(format(size)), (size === 1) ? 'Request.' : 'Requests.');
+  }
+
+  process.exitCode = (code === 1)
+    ? 1
+    : 0;
+};
